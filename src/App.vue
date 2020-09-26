e63dfdf925bae5b9590fdbd23a7809c3418fcd28
Guillermo Moreno <contact@memobrown.com>
2020-09-26 10:04:13 -0500

    fist commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld.vue";
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Hola Vue!'
-    }
+  name: "App",
+  components: {
+    HelloWorld
   }
-}
+};
 </script>
 
-<style lang="scss">
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
